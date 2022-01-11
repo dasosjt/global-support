eef6800ef1b69db7fd9f5f4748bd80c223e65a6b
diego.sosa <diego.sosa@backcountry.com>
2022-01-11 03:54:35 -0600

    update redis URL

--- src/redis/index.ts
+++ src/redis/index.ts
@@ -1,7 +1,7 @@
 import {createClient, RedisClientType} from '@node-redis/client';
 import IPlace from 'models/place.model';
 
-const client: RedisClientType = createClient();
+const client: RedisClientType = createClient({url: process.env.REDIS_URL || "redis://localhost:6379"});
 
 export const redisConnect = async (): Promise<string> => {
     await client.connect();
@@ -12,5 +12,4 @@ export const redisGeoDistance = async (place1: IPlace, place2: IPlace, unit: str
     client.geoAdd("geoDistance", {member: place1.name, latitude: place1.lat, longitude: place1.long});
     client.geoAdd("geoDistance", {member: place2.name, latitude: place2.lat, longitude: place2.long});
     return client.geoDist(place1.name, place2.name, unit);
-
 }
